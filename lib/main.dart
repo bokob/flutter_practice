b3bb89884fe94c300207bbacd43d2d32d3bd6df4
bokob <bmc0520@naver.com>
2023-03-17 13:33:07 +0900

    Scaffold 숙제 완료

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyAPP());
+}
+
+class MyAPP extends StatelessWidget {
+  const MyAPP({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title : Text("앱제목")),
+        body: Text("안녕"),
+        bottomNavigationBar: BottomAppBar(
+          child: SizedBox(
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page)],
+            ),
+          )
+      )
+      )
+      );
+  }
+}
