c3f36be71e57807668a289dc4a038fdf08268975
bokob <bmc0520@naver.com>
2023-03-19 14:25:03 +0900

    커스텀 위젯 만들기 숙제 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,65 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyAPP());
+  runApp(const MyApp());
 }
 
-class MyAPP extends StatelessWidget {
-  const MyAPP({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          titleTextStyle: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 20),
-          title : Row(
-           children: [Text("금호동3가"),
-             Icon(
-                 Icons.arrow_drop_down,
-                 color: Colors.black)]),
-          actions: [
-            Icon(Icons.search, color: Colors.black),
-            Icon(Icons.menu, color: Colors.black),
-            Icon(Icons.notifications_none, color: Colors.black)],
-          ),
-        body: Container(
-          margin: EdgeInsets.fromLTRB(5,5,5,5),
-          height: 135,
-          decoration: BoxDecoration(
-              border: Border(top:BorderSide(color: Colors.grey, width:1.0),
-              bottom:BorderSide(color: Colors.grey, width:1.0)
+        appBar: AppBar(title: Text('앱제목'),),
+        body: ListView(
+          children: [
+            Row(
+             children:[
+               Icon(Icons.account_circle, size: 50,),
+               Text('홍길동')
+             ], 
+            ),
+            Row(
+              children:[
+                Icon(Icons.account_circle, size: 50,),
+                Text('홍길동')
+              ],
+            ),
+            Row(
+              children:[
+                Icon(Icons.account_circle, size: 50,),
+                Text('홍길동')
+              ],
             )
-          ),
-          child: Row(
-            children: [
-              Container(
-                width:110,
-                padding: EdgeInsets.fromLTRB(3, 5, 3, 5),
-                child: Image.asset("camera.png")),
-              Flexible(
-                fit: FlexFit.tight,
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(2, 5, 2, 5),
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("캐논 DSLR 100D (단렌즈,",
-                              style: TextStyle(fontSize: 20)),
-                        Text("충전기 16기가SD 포함)",
-                            style: TextStyle(fontSize: 20)),
-                        Text("성동구 행당동 끌올 10분 전",
-                            style: TextStyle(
-                                fontSize: 13,
-                                color: Colors.grey)),
-                        Text("210,000원",
-                            style: TextStyle(
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold)),
-                        Flexible(
-                            fit: FlexFit.tight,
-                            child: Container(
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: [
-                                    Icon(Icons.favorite_border),
-                                    Text("4")
-                                  ],
-                                ),
-                              ),
-                        )
-                        ]
-                    ),
-                  ),
-                ),
-              )
-            ],
-          )
+          ],
+        ),
+        bottomNavigationBar: MyBottomAppBar(),
         )
-      )
-      );
+    );
   }
 }
+
+class MyBottomAppBar extends StatelessWidget {
+  const MyBottomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Icon(Icons.phone),
+            Icon(Icons.message_rounded),
+            Icon(Icons.contact_page)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
