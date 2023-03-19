10a0ef5305398aae74934d93d98fa20a3fa7be3f
bokob <bmc0520@naver.com>
2023-03-19 19:49:47 +0900

    혼자서 해볼 응용사항 완료

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,32 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var total = 3;
-  var name=['치킨집', '중국집', '피자집'];
+  List<String> name=<String>['치킨집', '중국집', '피자집'];
+  List<String> phone=<String>['010-1234-5678', '010-1357-2468', '010-2468-1357'];
 
   addOne(){
     setState((){
       total++;
       print(total);
+    });
+  }
+
+  addName(a){
+    setState(() {
+      name.add(a);
+    });
+  }
+
+  addPhone(a){
+    setState(() {
+      phone.add(a);
+    });
+  }
+
+  bool isSort=true;
+  nameSort(){
+    setState((){
+      name.sort((a, b) => isSort ? a.compareTo(b) : b.compareTo(a));
     });
   }
 
@@ -31,18 +51,35 @@ class _MyAppState extends State<MyApp> {
         floatingActionButton: FloatingActionButton(
           onPressed: (){
             showDialog(context: context, builder: (context){
-              return DialogUI(total:total, name:name ,addOne : addOne);
+              return DialogUI(addName : addName ,addOne : addOne,
+              nameSort : nameSort, addPhone:addPhone);
             });
           },
         ),
         appBar: AppBar(title: Text(total.toString())),
         body: ListView.builder(
-              itemCount: total,
+              itemCount: name.length,
               itemBuilder: (c, i){
-                return ListTile(
-                  leading: Icon(Icons.account_circle),
-                  title: Text(name[i]),
+                return Dismissible(
+                  key: Key(name[i]),
+                  child: ListTile(
+                    title: Text(name[i] + ' ' + phone[i]),
+                    leading: Icon(Icons.account_circle),
+                    trailing: TextButton(child: Text('삭제'), onPressed: (){
+                      setState(() {
+                        name.removeAt(i);
+                        total--;
+                      });
+                    })
+                  )
                 );
+                // return ListTile(
+                //   leading: Icon(Icons.account_circle),
+                //   title: Text(name[i]),
+                //   trailing: TextButton(child: Text('삭제'), onPressed: (){
+                //
+                //   },)
+                // );
               }
             ),
         );
@@ -50,11 +87,14 @@ class _MyAppState extends State<MyApp> {
 }
 
 class DialogUI extends StatelessWidget {
-  DialogUI({Key? key, this.total, this.name, this.addOne}) : super(key: key);
-  var total;
-  var name;
+  DialogUI({Key? key, this.addName, this.addOne, this.nameSort,
+  this.addPhone}) : super(key: key);
+  final addName;
   final addOne;
-  var inputData = TextEditingController();
+  final nameSort;
+  final addPhone;
+  var inputNameData = TextEditingController();
+  var inputPhoneData = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -64,16 +104,22 @@ class DialogUI extends StatelessWidget {
         height: 300,
         child: Column(
           children: [
-            TextField(controller: inputData,),
+            TextField(controller: inputNameData,),
+            TextField(controller: inputPhoneData,),
             TextButton( child: Text('완료'), onPressed:(){
               addOne();
-              name.add(inputData.text);
-              print(inputData.text);
-              print(name);
+              addName(inputNameData.text);
+              addPhone(inputPhoneData.text);
+              print(inputNameData.text);
+              print(inputPhoneData.text);
+              Navigator.pop(context);
               }),
             TextButton(
                 child: Text('취소'),
-                onPressed:(){ Navigator.pop(context); })
+                onPressed:(){ Navigator.pop(context); }),
+            TextButton(
+                child: Text('정렬'),
+                onPressed:(){ nameSort(); })
           ],
         ),
       ),
