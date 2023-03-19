16b6815d8c32644d7bf3e60301be6f713f7e39a0
bokob <bmc0520@naver.com>
2023-03-19 16:20:46 +0900

    Dialog 숙제 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+      home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -13,52 +16,56 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var name=['치킨집', '중국집', '피자집'];
-  var like=[0,0,0];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            showDialog(context: context, builder: (context){
+              return Dialog(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(15, 20, 15, 0),
+                  width:60,
+                  height: 150,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Contact',
+                        style: TextStyle(fontSize: 20,
+                            fontWeight: FontWeight.bold),),
+                      TextField(),
+                      Container(
+                        alignment: Alignment.bottomRight,
+                        height: 50,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(onPressed: (){
+                              Navigator.pop(context);
+                            }, child: Text('Cancel')),
+                            TextButton(onPressed: (){},
+                                child: Text('OK', textAlign: TextAlign.end,))
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ));
+            });
+          },
+        ),
         appBar: AppBar(title: Text('앱제목'),),
         body: ListView.builder(
               itemCount: 3,
               itemBuilder: (c, i){
                 return ListTile(
-                  leading: Text(like[i].toString()),
+                  leading: Icon(Icons.account_circle),
                   title: Text(name[i]),
-                  trailing: ElevatedButton(child: Text('좋아요'), onPressed: (){
-                    setState((){
-                      like[i]++;
-                    });
-                  },)
                 );
               }
             ),
-        bottomNavigationBar: MyBottomAppBar(),
-        )
-    );
+        );
   }
 }
-
-class MyBottomAppBar extends StatelessWidget {
-  const MyBottomAppBar({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomAppBar(
-      child: SizedBox(
-        height: 60,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(Icons.phone),
-            Icon(Icons.message_rounded),
-            Icon(Icons.contact_page)
-          ],
-        ),
-      ),
-    );
-  }
-}
-
 
 
